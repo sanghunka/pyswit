428d8ad9815862e662f75539d7729ffbd820b12a
sanghunka <sanghunka@gmail.com>
2022-10-08 15:04:53 +0900

    Add Pyswit, User, Message class

--- pyswit.py
+++ pyswit.py
@@ -22,3 +22,37 @@ class BaseAPI:
         if content_type:
             headers.update({"Content-Type": content_type})
         return headers
+
+
+class User(BaseAPI):
+    def info(self):
+        headers = self.get_headers()
+        r = requests.get(
+            "https://openapi.swit.io/v1/api/user.info",
+            headers=headers,
+            json=None,
+        )
+        return json.loads(r.content)
+
+
+class Message(BaseAPI):
+    def create(self, channel_id, content):
+        headers = self.get_headers(
+            accept="application/json", content_type="application/json"
+        )
+        data_obj = {"channel_id": channel_id, "content": content}
+
+        r = requests.post(
+            "https://openapi.swit.io/v1/api/message.create",
+            headers=headers,
+            json=data_obj,
+        )
+        return json.loads(r.content)
+
+
+class Pyswit:
+    def __init__(self, access_token):
+        api_args = {"access_token": access_token}
+
+        self.user = User(**api_args)
+        self.message = Message(**api_args)
