96cab86395929d837594409962cc1422f5a4f76d
Sanghun Kang <sanghunka@gmail.com>
2022-10-10 02:56:02 +0900

    Add message.list()

--- pyswit.py
+++ pyswit.py
@@ -92,6 +92,12 @@ class Message(BaseAPI):
         headers = self.get_headers()
         return self.get(url=url, headers=headers, params=params)
 
+    def list(self, channel_id: str, offset: str = None, limit: int = None):
+        params = self.params_to_dict(locals())
+        url = self.get_endpoint_url()
+        headers = self.get_headers(accept="application/json")
+        return self.get(url=url, headers=headers, params=params)
+
 
 class Pyswit:
     def __init__(self, access_token: str):
