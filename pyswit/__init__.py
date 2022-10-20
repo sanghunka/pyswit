debfce29c779a7f0f3cd512fba654c656b73208b
Sanghun Kang <sanghunka@gmail.com>
2022-10-21 00:09:02 +0900

    Add project.update()

--- pyswit/__init__.py
+++ pyswit/__init__.py
@@ -272,6 +272,14 @@ class Project(BaseAPI):
         headers = self.get_headers(accept="application/json")
         return self.get(url=url, headers=headers, params=params)
 
+    def update(self, id: str, name: str, description: str = None):
+        data = self.params_to_dict(locals())
+        url = self.get_endpoint_url()
+        headers = self.get_headers(
+            accept="application/json", content_type="application/json"
+        )
+        return self.post(url=url, headers=headers, data=data)
+
 
 class Task(BaseAPI):
     def create(
