53593c9ae56e439237bab97ab5a9f047402f34b0
Sanghun Kang <sanghunka@gmail.com>
2022-10-20 23:05:56 +0900

    Add workspace.user.update()

--- pyswit/__init__.py
+++ pyswit/__init__.py
@@ -66,6 +66,21 @@ class Workspace(BaseAPI):
             headers = self.get_headers(accept="application/json")
             return self.get(url=url, headers=headers, params=params)
 
+        def update(
+            self,
+            workspace_id: str,
+            department: str = None,
+            name: str = None,
+            status_msg: str = None,
+            tel: str = None,
+        ):
+            data = self.params_to_dict(locals())
+            url = self.get_endpoint_url()
+            headers = self.get_headers(
+                accept="application/json", content_type="application/json"
+            )
+            return self.post(url=url, headers=headers, data=data)
+
     def __init__(self, access_token: str):
         super().__init__(access_token=access_token)
         self.user = self.User(access_token=access_token, endpoint=self._class_name)
