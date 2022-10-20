970e9c0d825ee2cf7ebb81d190c828679b2abbd5
Sanghun Kang <sanghunka@gmail.com>
2022-10-20 22:37:42 +0900

    Add workspace.user & workspace.user.info()

--- pyswit/__init__.py
+++ pyswit/__init__.py
@@ -53,6 +53,17 @@ class User(BaseAPI):
 
 
 class Workspace(BaseAPI):
+    class User(BaseAPI):
+        def info(self, workspace_id: str, user_id: str = None):
+            params = self.params_to_dict(locals())
+            url = self.get_endpoint_url()
+            headers = self.get_headers(accept="application/json")
+            return self.get(url=url, headers=headers, params=params)
+
+    def __init__(self, access_token: str):
+        super().__init__(access_token=access_token)
+        self.user = self.User(access_token=access_token, endpoint=self._class_name)
+
     def info(self, id: str):
         params = self.params_to_dict(locals())
         url = self.get_endpoint_url()
