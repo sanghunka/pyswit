25cb5dcc899578204e490d52591e96f6fabd8961
Sanghun Kang <sanghunka@gmail.com>
2022-10-28 02:20:47 +0900

    Add Project.bucket.list()

--- pyswit/project.py
+++ pyswit/project.py
@@ -24,6 +24,12 @@ class Project(BaseAPI):
             headers = self.get_headers(accept="application/json")
             return self.get(url=url, headers=headers, params=params)
 
+        def list(self, project_id: str, limit: int = None, offset: str = None):
+            params = self.params_to_dict(locals())
+            url = self.get_endpoint_url()
+            headers = self.get_headers(accept="application/json")
+            return self.get(url=url, headers=headers, params=params)
+
     def __init__(self, access_token: str):
         super().__init__(access_token=access_token)
         self.user = self.User(access_token=access_token, endpoint=self._class_name)
