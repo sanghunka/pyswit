38b4b7aefc463f2524cb4fb681cb61bdf1777baa
Sanghun Kang <sanghunka@gmail.com>
2022-10-29 23:17:33 +0900

    Add Idea.create()

--- pyswit/idea.py
+++ pyswit/idea.py
@@ -16,3 +16,12 @@ class Idea(BaseAPI):
         self.reaction = self.Reaction(
             access_token=access_token, endpoint=self._class_name
         )
+
+    def create(self, channel_id: str, content: str):
+        data = self.params_to_dict(locals())
+        url = self.get_endpoint_url()
+        headers = self.get_headers(
+            accept="application/json", content_type="application/json"
+        )
+        return self.post(url=url, headers=headers, data=data)
+
